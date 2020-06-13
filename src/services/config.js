fe61f5add16a06b96328e52983371dd89394f6ad
foolsogood <851809776@qq.com>
2020-06-13 18:14:23 +0800

    up

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,9 @@
-export let server = "http://127.0.0.1:9100";
-// export let server = "http://47.112.195.142:9100";
+export let server = "http://47.112.195.142:9100";
 
-if (process.env.NODE_ENV === "production") {
-  server = "http://47.112.195.142:9100";
-}
+// export let server = "http://127.0.0.1:9100";
+// if (process.env.NODE_ENV === "production") {
+//   server = "http://47.112.195.142:9100";
+// }
 export default {
   server
 };
